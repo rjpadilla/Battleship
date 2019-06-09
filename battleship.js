50647947f21e1458b5cbbf1a45885264469976ae
Raul Padilla <raulpadilla112@gmail.com>
2019-06-09 01:32:55 -0400

    Added sunk function.

--- battleship.js
+++ battleship.js
@@ -20,25 +20,37 @@ var model = {
     boardSize: 7,
     numShips: 3,
     shipLength: 3,
-    shipsSunk:0,
+    shipsSunk: 0,
 
     ships:
         [{ locations: ["06", "16", "26"], hits: ["hit", "", ""] },
         { locations: ["24", "34", "44"], hits: ["hit", "", ""] },
         { locations: ["10", "11", "12"], hits: ["hit", "", ""] }],
 
-        fire: function(guess){
-            for(var i =0; i<this.numShips; i++){
-                var ship = this.ship[i];
-                locations = ship.locations;
-                var index = locations.indexOf(guess);
-                if(index >=0){
-                    ships.hits[index] = "hit";
-                    return true;
+    fire: function (guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ship[i];
+            locations = ship.locations;
+            var index = locations.indexOf(guess);
+            if (index >= 0) {
+                ships.hits[index] = "hit";
+                if (this.isSunk(ship)) {
+                    this.shipsSunk++;
                 }
+                return true;
             }
-            return false;
         }
+        return false;
+    },
+
+    isSunk: function (ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 };
