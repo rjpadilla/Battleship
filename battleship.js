b29202677ea03d2adb76ed69615287abfce696f6
Raul Padilla <raulpadilla112@gmail.com>
2019-06-11 01:30:58 -0400

    Added controller.

--- battleship.js
+++ battleship.js
@@ -61,6 +61,22 @@ var model = {
 };
 
 
+var controller = {
+    guesses: 0,
+
+    processGuess: function (guess) {
+        var location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            var hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+            }
+        }
+    }
+}
+
+/*
 model.fire("54"); // miss
 
 model.fire("06"); // hit
@@ -75,6 +91,7 @@ model.fire("44"); // hit
 model.fire("12"); // hit
 model.fire("11"); // hit
 model.fire("02"); // hit
+*/
 
 function parseGuess(guess) {
     var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
@@ -97,12 +114,23 @@ function parseGuess(guess) {
     return false;
 }
 
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
+/*
 console.log(parseGuess("A0"));
 console.log(parseGuess("B6"));
 console.log(parseGuess("G3"));
 console.log(parseGuess("H0"));
 console.log(parseGuess("A7"));
-/*
+
 view.displayMiss("00");
 view.displayHit("34");
 view.displayMiss("55");
